b4bed01842fff8334593e49ceef2b07e0ce516dc
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-05 16:52:50 +0000

    init

--- js/script.js
+++ js/script.js
@@ -5,54 +5,25 @@
 
 // This file contains the JS functions for index.html
 
+"use-strict"
 
 /**
 * This function check the type of triangle
 */
 // eslint-disable-next-line no-unused-vars
-function checkTypeOfTriangle () {
- // input
-const sideA = parseFloat(document.getElementById('side-a').value)
-const sideB = parseFloat(document.getElementById('side-b').value)
-const sideC = parseFloat(document.getElementById('side-c').value)
+function calculateNumber () {
+  //input 
+  const numberOne = parseFloat(document.getElementById('number-one').value)
+  const numberTwo = parseFloat(document,getElementById('number-two').value)
+  let counter = 0
+  let answer = 0 
 
-
- // using the cosine law
-const angleA =
-  Math.acos((sideB ** 2 + sideC ** 2 - sideA ** 2) / (2 * sideB * sideC)) *
-  (180 / Math.PI)
-const angleB =
-  Math.acos((sideC ** 2 + sideA ** 2 - sideB ** 2) / (2 * sideC * sideA)) *
-  (180 / Math.PI)
-const angleC =
-  Math.acos((sideA ** 2 + sideB ** 2 - sideC ** 2) / (2 * sideA * sideB)) *
-  (180 / Math.PI)
-
-
-const sumOfAngles =
-  Number(angleA.toFixed(2)) +
-  Number(angleB.toFixed(2)) +
-  Number(angleC.toFixed(2))
-
-
-if (sumOfAngles === 180) {
-   // process
-  if (sideA === sideB && sideA === sideC && sideB === sideC) {
-    // output
-    document.getElementById('answer').innerHTML =
-      'This is an Equilateral Triangle.'
-  } else if (sideA === sideC || sideA === sideB || sideB === sideC) {
-    // output
-    document.getElementById('answer').innerHTML =
-      'This is an Isosceles Triangle.'
-  } else {
-    // output
-    document.getElementById('answer').innerHTML =
-      'This is a Scalene Triangle.'
+  //process
+  while (counter < secondNumber ) {
+    counter = counter + 1
+    answer = answer + firstNumber 
   }
-} else {
+
   // output
-  document.getElementById('answer').innerHTML = 'This is not a triangle.'
-
-  }
+  document.getElementById('calculate').innerHTML = 'Your answer is:' + answer
 }
